4747f88686ab44ecec61438a8324b69090a27c74
xiong <xiongjinhaimao@126.com>
2018-05-11 13:10:58 +0800

    初始化

---
+++ wechat/WeChatServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @File: WeChatServiceProvider.php
+ * @Author: xiongjinhai
+ * @Email:562740366@qq.com
+ * @Date: 2018/5/11上午9:57
+ * @Version:Version:1.1 2017 by www.dsweixin.com All Rights Reserver
+ */
+
+namespace Pay\WeChat;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class WeChatServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        echo 33;exit;
+    }
+}
