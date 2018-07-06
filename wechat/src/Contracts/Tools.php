b51194294763b7985bffce74c8207ee82ae3fbd4
xiong <xiongjinhaimao@126.com>
2018-07-06 09:48:04 +0800

    禁止引用外部xml实体

--- wechat/src/Contracts/Tools.php
+++ wechat/src/Contracts/Tools.php
@@ -127,6 +127,10 @@ class Tools
      */
     public static function xml2arr($xml)
     {
+        //将XML转为array
+        //禁止引用外部xml实体
+        libxml_disable_entity_loader(true); //这句导致出现上述问题
+
         return json_decode(self::arr2json(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
     }
 
